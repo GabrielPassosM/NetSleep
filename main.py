9f6236abed334ab6336c356f28d20836c0a2be05
gabmartins <gabrielpassosmartins@gmail.com>
2025-02-13 17:22:00 -0300

    fix: enchance detection when eyes are fully closed

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ RIGHT_EYE = list(range(36, 42))
 
 cap = cv2.VideoCapture(0)
 fps = cap.get(cv2.CAP_PROP_FPS) or 30
-EAR_THRESHOLD = 0.20
+EAR_THRESHOLD = 0.25
 TEMPO_LIMITE_SEGUNDOS = 2
 FRAME_THRESHOLD = int(TEMPO_LIMITE_SEGUNDOS * fps)
 
@@ -36,11 +36,11 @@ def eye_aspect_ratio(eye):
 
 def pause_media() -> None:
     subprocess.run(pause_command)
-    print("PAUSADO")
 
 
 frames_closed = 0
 paused = False
+face_detected = False
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -49,34 +49,45 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
 
-    for face in faces:
-        landmarks = predictor(gray, face)
-        landmarks = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(68)])
-
-        left_eye = landmarks[LEFT_EYE]
-        right_eye = landmarks[RIGHT_EYE]
-
-        left_ear = eye_aspect_ratio(left_eye)
-        right_ear = eye_aspect_ratio(right_eye)
-        ear = (left_ear + right_ear) / 2.0
-
-
-        cv2.polylines(frame, [left_eye], True, (0, 255, 0), 1)
-        cv2.polylines(frame, [right_eye], True, (0, 255, 0), 1)
-
-        if ear < EAR_THRESHOLD:
+    if len(faces) == 0:
+        if face_detected:
             frames_closed += 1
-            if frames_closed >= FRAME_THRESHOLD:
+            if frames_closed > FRAME_THRESHOLD:
                 pause_media()
-                paused = True
-                break
         else:
             frames_closed = 0
+    else:
+        face_detected = True
+        for face in faces:
+            landmarks = predictor(gray, face)
+            landmarks = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(68)])
 
-    cv2.imshow("Deteccao de Olhos", frame)
+            left_eye = landmarks[LEFT_EYE]
+            right_eye = landmarks[RIGHT_EYE]
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            left_ear = eye_aspect_ratio(left_eye)
+            right_ear = eye_aspect_ratio(right_eye)
+            ear = (left_ear + right_ear) / 2.0
+
+            # Uncomment to show eye tracking
+            # cv2.polylines(frame, [left_eye], True, (0, 255, 0), 1)
+            # cv2.polylines(frame, [right_eye], True, (0, 255, 0), 1)
+
+            if ear < EAR_THRESHOLD:
+                frames_closed += 1
+                if frames_closed >= FRAME_THRESHOLD:
+                    pause_media()
+                    paused = True
+                    print("PAUSADO")
+                    break
+            else:
+                frames_closed = 0
+
+    # Uncomment to show webcam window
+    # cv2.imshow("Deteccao de Olhos", frame)
+
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
     if paused:
         break
